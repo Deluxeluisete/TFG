f9772d437dc856bd58db33bd3766aad510fb87d1
Deluxeluisete <luis.suarez@iesdoctorbalmis.com>
2023-06-11 21:14:55 +0200

    arreglo error de cors

--- MiViaje/src/app/interceptors/base-url.interceptor.ts
+++ MiViaje/src/app/interceptors/base-url.interceptor.ts
@@ -5,10 +5,10 @@ export const baseUrlInterceptor: HttpInterceptorFn = (req, next) => {
   let serverUrl: string;
   if (isDevMode()) {
     // App in development mode
-    serverUrl = 'http://localhost:3000'; // Development server url
+    serverUrl = ''; // Development server url
   } else {
     // App in production mode
-    serverUrl = 'http://localhost:3000'; // Production server url
+    serverUrl = ''; // Production server url
   }
   const reqClone = req.clone({
     url: `${serverUrl}/${req.url}`,
